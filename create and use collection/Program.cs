ec9d1fca16e53ce8a418c23046326de5b8cc3b53
belu82 <44766864+belu82@users.noreply.github.com>
2019-10-29 15:19:41 +0100

    Add project files.

---
+++ create and use collection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace create_and_use_collection
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+
+            while (queue.Count > 0)
+            {
+                object obj = queue.Dequeue();
+
+                Console.WriteLine("From Queue: {0}", obj);
+            }
+
+        }
+    }
+}
