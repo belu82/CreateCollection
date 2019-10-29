eb69052b4b535afae433554a0a2b914783aa2dd5
belu82 <44766864+belu82@users.noreply.github.com>
2019-10-29 15:26:57 +0100

    Excercise 2 is ready

--- create and use collection/Program.cs
+++ create and use collection/Program.cs
@@ -23,6 +23,19 @@ namespace create_and_use_collection
                 Console.WriteLine("From Queue: {0}", obj);
             }
 
+            Stack stacks = new Stack();
+            stacks.Push("one");
+            stacks.Push("two");
+            stacks.Push("three");
+            stacks.Push("four");
+
+            while(stacks.Count > 0)
+            {
+                Object obj = stacks.Pop();
+                Console.WriteLine("From stack: {0}", obj);
+            }
+
+
         }
     }
 }
