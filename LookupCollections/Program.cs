5144a9e06f773aca601ae441f5a53083761c39bd
belu82 <44766864+belu82@users.noreply.github.com>
2019-10-29 17:50:25 +0100

    exercise 3 ready

---
+++ LookupCollections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+
+
+namespace LookupCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListDictionary lista = new ListDictionary(new CaseInsensitiveComparer(CultureInfo.InvariantCulture));
+            lista.Add("Estados Unidos", "United States");
+            lista.Add("Canadá", "Canada");
+            lista.Add("España", "Spain");
+            Console.WriteLine(lista["españa"]);
+            Console.WriteLine(lista["CANADÁ"]);
+            Console.Read();
+        }
+    }
+}
